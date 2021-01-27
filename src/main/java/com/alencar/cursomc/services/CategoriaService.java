193a7d7e7a8dde15c11d553f3bb1d5594cbac8ae
DanielAllencar <daniel.alencar.fatec@gmail.com>
2021-01-26 22:39:22 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/alencar/cursomc/services/CategoriaService.java
+++ src/main/java/com/alencar/cursomc/services/CategoriaService.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.alencar.cursomc.domain.Categoria;
 import com.alencar.cursomc.repositories.CategoriaRepository;
+import com.alencar.cursomc.services.exceptions.ObjectNotFoundException;
+
 import java.util.Optional;
 
 @Service
@@ -13,7 +15,7 @@ public class CategoriaService {
 	private CategoriaRepository repo;
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null); 
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado ! Id: " + id + ", Tipo: " + Categoria.class.getName())); 
 
 		
 	}
