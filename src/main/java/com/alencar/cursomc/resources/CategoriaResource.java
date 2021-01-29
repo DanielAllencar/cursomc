135c8f77bb81d8de3e1460a38ad1a60e4632fc9e
DanielAllencar <daniel.alencar.fatec@gmail.com>
2021-01-29 01:16:52 -0300

    Inserindo uma nova Categoria com Post

--- src/main/java/com/alencar/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/alencar/cursomc/resources/CategoriaResource.java
@@ -1,11 +1,16 @@
 package com.alencar.cursomc.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
 import com.alencar.cursomc.domain.Categoria;
 import com.alencar.cursomc.services.CategoriaService;
 
@@ -16,12 +21,21 @@ public class CategoriaResource {
 	@Autowired
 	private CategoriaService service;
 	
+	//metodo de pesquisa por codigo                               
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		Categoria obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);		
-		
-				
+	}
+	
+	//Mapeamento que indica um metodo de inserir 
+	@RequestMapping(method = RequestMethod.POST)
+	//metodo post de inserir no banco de dados que chama na pasta de services
+	//@RequestBody faz com que o Json seja convertido para o objeto java automaticamente 
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj){ 
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 
 	
--- src/main/java/com/alencar/cursomc/services/CategoriaService.java
+++ src/main/java/com/alencar/cursomc/services/CategoriaService.java
@@ -11,14 +11,20 @@ import java.util.Optional;
 @Service
 public class CategoriaService {
 	
+	
 	@Autowired
 	private CategoriaRepository repo;
+	
+	//metodo de pesquisa por codigo no banco de dados
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado ! Id: " + id + ", Tipo: " + Categoria.class.getName())); 
-
-		
 	}
 	
+	//metodo de inserir no banco de dados
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
 
 }
