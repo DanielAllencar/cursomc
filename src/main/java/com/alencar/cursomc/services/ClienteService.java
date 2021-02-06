193b5d7950f104d12fa6daa7eb163634b03c3b40
DanielAllencar <daniel.alencar.fatec@gmail.com>
2021-02-06 20:16:50 -0300

    Inserindo um novo cliente com POST

--- src/main/java/com/alencar/cursomc/services/ClienteService.java
+++ src/main/java/com/alencar/cursomc/services/ClienteService.java
@@ -1,31 +1,48 @@
 package com.alencar.cursomc.services;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.alencar.cursomc.domain.Cidade;
 import com.alencar.cursomc.domain.Cliente;
+import com.alencar.cursomc.domain.Endereco;
+import com.alencar.cursomc.domain.enums.TipoCliente;
 import com.alencar.cursomc.dto.ClienteDTO;
+import com.alencar.cursomc.dto.ClienteNewDTO;
 import com.alencar.cursomc.repositories.ClienteRepository;
+import com.alencar.cursomc.repositories.EnderecoRepository;
 import com.alencar.cursomc.services.exceptions.DataIntegrityException;
 import com.alencar.cursomc.services.exceptions.ObjectNotFoundException;
 
-
-import java.util.List;
-import java.util.Optional;
-
 @Service
 public class ClienteService {
-	
+
 	@Autowired
 	private ClienteRepository repo;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+	
 	public Cliente find (Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado ! Id: " + id + ", Tipo: " + Cliente.class.getName()));
-		
+	}
+	
+	//metodo de inserir no banco de dados
+	@Transactional
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = repo.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		return obj;
 	}
 	
 	//metodo de atualizar 
@@ -60,6 +77,21 @@ public class ClienteService {
 		
 	public Cliente fromDTO(ClienteDTO objDto) {
 		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+	
+	public Cliente fromDTO(ClienteNewDTO objDto) {
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(), TipoCliente.toEnum(objDto.getTipo()));
+		Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(), objDto.getBairro(), objDto.getCep(), cli, cid);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDto.getTelefone1());
+		if(objDto.getTelefone2()!=null) {
+			cli.getTelefones().add(objDto.getTelefone2());
+		}
+		if(objDto.getTelefone3()!=null) {
+			cli.getTelefones().add(objDto.getTelefone3());
+		}
+		return cli;
 	}
 	
 	//metodo auxiliar que busca o objeto no banco de dados e altera os dados novos mais mantendo os outros dados intactos
